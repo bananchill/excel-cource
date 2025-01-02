0c4df5c7899eafc83b381d9391e2badc72019443
Malsim Orlov <maks.maksim010607@yandex.ru>
2025-01-02 19:49:59 +0300

    Finish base framework functional

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -9,7 +9,7 @@ export class Excel {
   getRoot() {
     const $root = $.create('div', 'excel');
 
-    this.components.forEach((Component) => {
+    this.components = this.components.map((Component) => {
       const $el = $.create('div', Component.className);
 
       const component = new Component($el);
@@ -17,14 +17,18 @@ export class Excel {
       $el.html(component.toHTML());
 
       $root.append($el);
+
+      return component;
     });
 
     return $root;
   }
 
   render() {
-    // this.$el.insertAdjacentHTML('afterbegin', this.$el);
+    this.$el.append(this.getRoot());
 
-    this.$el.appendChild(this.getRoot());
+    this.components.forEach((component) => {
+      component.init();
+    });
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -18,12 +18,21 @@ class Dom {
     return this;
   }
 
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback);
+  }
+
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback);
+  }
+
   append(node) {
     if (node instanceof Dom) {
       node = node.$el;
     }
+
     if (Element.prototype.append) {
-      this.$el.append(node.$el);
+      this.$el.append(node);
     } else {
       this.$el.appendChild(node.$el);
     }
